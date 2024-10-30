d7d713e79ced5ef8b03a3c002ecad2babba30d4d
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-10-30 13:57:17 +0800

    test unity download & install

---
+++ Assets/Editor/EditorProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+
+public class EditorProcess : Editor
+{
+    [MenuItem("Tools/Roslyn Analyzer", false, 1)]
+    public static void CodeAnalyze()
+    {
+        Debug.Log("Call Analyzer");
+        var processInfo = new ProcessStartInfo("cmd.exe", "dotnet build >> output.txt");
+        processInfo.CreateNoWindow = false;
+        processInfo.UseShellExecute = false;
+
+        var process = Process.Start(processInfo);
+
+        process.WaitForExit();
+        //process.Close();
+    }
+}
--- Assets/EmptyIssue.cs
+++ Assets/EmptyIssue.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,12 @@ public class EmptyIssue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
