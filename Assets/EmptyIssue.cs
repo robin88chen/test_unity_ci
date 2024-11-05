cfc34269c9f6692184cf7a22b9f56a11b5f5481b
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-11-05 16:45:47 +0800

    test for log stack trace

--- Assets/EmptyIssue.cs
+++ Assets/EmptyIssue.cs
@@ -7,7 +7,8 @@ public class EmptyIssue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Application.SetStackTraceLogType(LogType.Error, StackTraceLogType.Full);
+        Debug.LogError("This is an error message");
     }
 
     // Update is called once per frame
